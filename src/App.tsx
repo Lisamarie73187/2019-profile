2055378b81789d7102426f1486ba9645a93ddfe8
Lisa Herzberg <lisamarieherzberg@gmail.com>
2019-10-17 19:04:23 -0700

    update footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import {Header} from './components/Header';
 import {RouterHome} from './RouterHome';
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <div className="homeWrapper">
-        <Header />
-        <RouterHome />
-        <Footer />
+    <div>
+      <div className="App">
+        <div className="homeWrapper">
+          <Header />
+          <RouterHome />
+        </div>
       </div>
+        <Footer />
     </div>
   );
 };
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import profilePic from '../assets/ProfilePic.jpg';
+import {Link} from 'react-router-dom';
 
 export const Footer = () => {
   return (
-    <div className="aboutMeWrapper">
-      <div className="copy">©2019 Lisa Herzberg</div>
+    <div className="footerWrapper">
+      <div className="footerLinks">
+        <Link to="/About" className="noDecor">
+          <div className="footerLink">About</div>
+        </Link>
+        <Link to="/" className="noDecor">
+          <div className="footerLink">Projects</div>
+        </Link>
+        <a href="https://indd.adobe.com/view/364c2a85-eab2-417b-ac78-b5b7b5d05670" target="_blank" className="resumeLink">
+          <div className="footerLink">Resume</div>
+        </a>
+      </div>
+      <div className="copyWrapper">
+        {'\u00A9'}
+        <span className="copy">2019 Lisa Herzberg</span>
+      </div>
     </div>
   );
 };
