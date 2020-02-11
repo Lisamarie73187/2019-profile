a6d4c328ada72b2be5263a77ecf461e82361b2db
Lisa Herzberg <lisamarieherzberg@gmail.com>
2020-02-10 20:08:24 -0700

    updated profile

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,8 +10,8 @@ export const About = () => {
         <p>
           Hello! I came to discover my passion in technology later in life. I first inspired to succeed in the health and wellness field, which I soon found out that not only
           was I not satisfied I was not challenged enough. As I was trying to find my perfect career I started taking classes in computer programing and design, from there I
-          enrolled in Dev Mountain and the rest is history. I was able to use my creativity and outside of the box thinking to excel throughout the course. My craving for
-          constant learning and a challenging work made this career the perfect fit and due to my previous experience I was able to apply my knowledge of consumer and customer satisfaction into
+          enrolled in the Dev Mountain boot camp and the rest is history. I was able to use my creativity and outside of the box thinking to excel throughout the course. My craving for
+          constant learning and challenging work made this career the perfect fit and due to my previous experience I was able to apply my knowledge of consumer and customer satisfaction into
           my work.
         </p>
         <p>
