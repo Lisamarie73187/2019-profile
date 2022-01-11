8847d479e2b19bf25ad98890834195d6f49df1d1
Lisa Herzberg <lisaherzberg@Lisas-MBP.attlocal.net>
2022-01-10 20:29:17 -0800

    updated about me

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,13 +11,12 @@ export const About = () => {
           Hello! I came to discover my passion in technology later in life. I first inspired to succeed in the health and wellness field, which I soon found out that not only
           was I not satisfied I was not challenged enough. As I was trying to find my perfect career I started taking classes in computer programing and design, from there I
           enrolled in the Dev Mountain boot camp and the rest is history. I was able to use my creativity and outside of the box thinking to excel throughout the course. My craving for
-          constant learning and challenging work made this career the perfect fit and due to my previous experience I was able to apply my knowledge of consumer and customer satisfaction into
+          constant learning and challenging work made this career the perfect fit and due to my previous experience I was able to apply my knowledge of customer satisfaction into
           my work.
         </p>
         <p>
-          When I am not at my computer coding, you will typically find me behind crafts or enjoying the outdoors. I love learning and finding new activities and hobbies. I recently
-          have picked up knitting and before that I was making jewelry. If you see me outdoors, it will most likely be on bike, running, hiking,
-          or kayaking. Feel free to message me I love meeting new people!
+          When I am not at my computer coding, you will typically find me enjoying the outdoors or doing some sort of craft. I love learning and finding new activities and hobbies. I recently
+          have picked up knitting and macrame. If you see me outdoors, it will most likely be running, hiking, or playing beach volleyball. Feel free to message me I love meeting new people!
         </p>
       </div>
     </div>
